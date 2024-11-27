6e47577588c3a11609a8b34da60d1bfc0a5f9eb8
giyomdabin <dabinjang01@gmail.com>
2024-11-27 20:55:04 +0900

    Initial commit

---
+++ src/main/java/sch/youfitserver/YoufitServerApplication.java
@@ -0,0 +1,13 @@
+package sch.youfitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoufitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoufitServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sch/youfitserver/YoufitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sch.youfitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoufitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
