bc48c44f7f686c04bd2b17f6aa5a315bfcd8b753
giyomdabin <dabinjang01@gmail.com>
2024-11-28 23:46:08 +0900

    feat: 각 체육시설의 상세 정보 가져오기

--- src/main/java/sch/youfitserver/center/controller/CenterController.java
+++ src/main/java/sch/youfitserver/center/controller/CenterController.java
@@ -3,7 +3,9 @@ package sch.youfitserver.center.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import sch.youfitserver.center.dto.CenterDto;
 import sch.youfitserver.center.dto.CenterListDto;
 import sch.youfitserver.center.service.CenterService;
 
@@ -16,9 +18,17 @@ public class CenterController {
 
     private final CenterService centerService;
 
+    // 체육센터 전체 리스트
     @GetMapping("/center")
     public ResponseEntity<List<CenterListDto>> getCenters() throws IOException {
+        List<CenterListDto> centers = centerService.getCenters();
+        return ResponseEntity.ok(centers);
+    }
 
-        return centerService.getCenters();
+    // 체육센터의 상세 정보
+    @GetMapping("/center/{centerId}")
+    public ResponseEntity<CenterDto> getCenter(@PathVariable Long centerId) throws IOException {
+        CenterDto centerDto = centerService.getCenter(centerId);
+        return ResponseEntity.ok(centerDto);
     }
 }
--- src/main/java/sch/youfitserver/center/service/CenterService.java
+++ src/main/java/sch/youfitserver/center/service/CenterService.java
@@ -4,11 +4,11 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import sch.youfitserver.center.dto.CenterDto;
 import sch.youfitserver.center.dto.CenterListDto;
 import sch.youfitserver.center.entity.Center;
 import sch.youfitserver.center.repository.CenterRepository;
 
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,24 +19,36 @@ public class CenterService {
     private final CenterRepository centerRepository;
 
     @Transactional
-    public ResponseEntity<List<CenterListDto>> getCenters() {
+    public List<CenterListDto> getCenters() {
         List<Center> centers = centerRepository.findAll();
 
-        List<CenterListDto> centerList = centers.stream()
+        return centers.stream()
                 .map(this::convertToCenterListDto)
                 .collect(Collectors.toList());
-
-        return ResponseEntity.ok(centerList);
     }
 
     private CenterListDto convertToCenterListDto(Center center) {
-
         // Center -> CenterListDTO 변환
         return CenterListDto.builder()
                 .centerId(center.getCenterId())
                 .centerName(center.getCenterName())
                 .lat(center.getLat())
                 .lot(center.getLot())
+                .build();
+    }
+
+    public CenterDto getCenter(Long centerId) {
+        Center center = centerRepository.findById(centerId)
+                .orElseThrow(() -> new IllegalArgumentException("${centerId}에 해당하는 체육센터를 찾을 수 없습니다."));
+
+        return CenterDto.builder()
+                .region(center.getRegion())
+                .location(center.getLocation())
+                .centerName(center.getCenterName())
+                .lat(center.getLat())
+                .lot(center.getLot())
+                .addr(center.getAddr())
+                .tel(center.getTel())
                 .build();
     }
 }
