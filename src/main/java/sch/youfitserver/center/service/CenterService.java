058f3a45ab78ecc4ecb630da572f6b6b0a9cd1b1
giyomdabin <dabinjang01@gmail.com>
2024-11-29 01:49:36 +0900

    feat: custom exception 추가

--- src/main/java/sch/youfitserver/center/service/CenterService.java
+++ src/main/java/sch/youfitserver/center/service/CenterService.java
@@ -10,6 +10,7 @@ import sch.youfitserver.center.entity.Center;
 import sch.youfitserver.center.repository.CenterRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -39,7 +40,7 @@ public class CenterService {
 
     public CenterDto getCenter(Long centerId) {
         Center center = centerRepository.findById(centerId)
-                .orElseThrow(() -> new IllegalArgumentException("${centerId}에 해당하는 체육센터를 찾을 수 없습니다."));
+                .orElseThrow(() -> new NoSuchElementException("해당하는 체육센터를 찾을 수 없습니다."));
 
         return CenterDto.builder()
                 .region(center.getRegion())
