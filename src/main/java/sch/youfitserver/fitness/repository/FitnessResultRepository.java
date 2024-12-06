bad82c43955dba83a5b09541973cc3318f96d27c
seunggyu <seungg8361@nave.com>
2024-12-06 16:56:26 +0900

    feat: home화면 기능을 추가함

--- src/main/java/sch/youfitserver/fitness/repository/FitnessResultRepository.java
+++ src/main/java/sch/youfitserver/fitness/repository/FitnessResultRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sch.youfitserver.fitness.entity.FitnessResult;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface FitnessResultRepository extends JpaRepository<FitnessResult, Long> {
-    Optional<FitnessResult> findByFitness_fitnessId(Long fitnessId);
+    List<FitnessResult> findByFitness_fitnessId(Long fitnessId);
 }
